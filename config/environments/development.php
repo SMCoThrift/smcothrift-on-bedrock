5514c502c2ff64932a7e4df3f28cae5396f1e44d
Michael Wender <michael@michaelwender.com>
2025-08-14 13:38:34 -0400

    Adding REMOTE_UPLOADS_URL

--- config/environments/development.php
+++ config/environments/development.php
@@ -15,8 +15,19 @@ Config::define('WP_DEBUG_LOG', env('WP_DEBUG_LOG') ?? true);
 Config::define('WP_DISABLE_FATAL_ERROR_HANDLER', true);
 Config::define('SCRIPT_DEBUG', true);
 Config::define('DISALLOW_INDEXING', true);
+Config::define('REMOTE_UPLOADS_URL', env('REMOTE_UPLOADS_URL') ?? false);
 
 ini_set('display_errors', '1');
 
 // Enable plugin and theme updates and installation from the admin
 Config::define('DISALLOW_FILE_MODS', false);
+
+
+/**
+ * Redirects wp-content/uploads URLs to the production server in local environments.
+ */
+add_filter( 'upload_dir', function( $dirs ) {
+	if( REMOTE_UPLOADS_URL )
+		$dirs['baseurl'] = REMOTE_UPLOADS_URL; // 'https://example.com/wp-content/uploads';
+  return $dirs;
+} );
